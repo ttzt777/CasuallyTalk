bd9297b1f6cbe8ca07ccc36af1866949489d18f8
zhaotao <zhaotao1206@163.com>
2018-03-28 16:43:53 +0800

    first commit

---
+++ android/app/src/main/java/top/bear3/casuallytalk/MainActivity.java
@@ -0,0 +1,14 @@
+package top.bear3.casuallytalk;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
